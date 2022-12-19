b10e38201c143cb91abf70738a7037598409f52e
KNLOD <gigoff5@gmail.com>
2022-12-19 08:51:06 +0300

    moved files

--- src/websocket_client.jl
+++ src/websocket_client.jl
@@ -7,14 +7,6 @@ const HOST = "0.0.0.0"
 const PORT = "12346"
 
 
-#server = WebSockets.listen!(HOST, PORT) do websocket
-#
-#	for message in websocket
-#		send(websocket, message)
-#	end
-#end
-	
-
 WebSockets.open("ws://"*HOST*":"*PORT) do websocket
 	for message in websocket
 		@info message
